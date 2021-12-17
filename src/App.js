34880ffc6573c266c42b326fa741bbcecf26c66a
Joseph Huang <joseph.lite.huang@gmail.com>
2021-12-17 08:55:56 -0600

    Update App with Custom Edit Validation

--- src/App.js
+++ src/App.js
@@ -45,6 +45,13 @@ class App extends React.Component {
       company: '',
       phone: '',
       email: ''
+    },
+    editFormErrors: {
+      firstName: '',
+      lastName: '',
+      company: '',
+      phone: '',
+      email: ''
     }
   }
 
@@ -201,6 +208,13 @@ class App extends React.Component {
     console.log(`Submitting edit for contact id ${contactId}`)
     console.log(this.state.editContactData)
 
+    let validationErrors = this.validateContact(this.state.editContactData)
+    if (!validationErrors.isValid) {
+      console.log("Edited contact is invalid. Reporting errors.")
+      this.setState({ editFormErrors: validationErrors })
+      return
+    }
+
     fetch(SERVICE_URL + '/contact/' + contactId, {
       method: 'PUT',
       headers: {
@@ -211,7 +225,7 @@ class App extends React.Component {
       .then(response => response.json())
       .then(data => {
         console.log('Success:', data);
-        this.setState({ showEditModal: false })
+        this.setState({ showEditModal: false, editFormErrors: validationErrors })
         this.loadContactData();
       })
       .catch((error) => {
@@ -273,7 +287,8 @@ class App extends React.Component {
           handleSubmit={this.handleEditFormSubmit}
           handleChange={this.handleEditFormChange}
           handleClose={this.handleEditModalClose}
-          contactData={this.state.editContactData} />
+          contactData={this.state.editContactData}
+          contactErrors={this.state.editFormErrors} />
       </Container>
     );
   }
